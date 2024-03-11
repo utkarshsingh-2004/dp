064ddeb61f98c5e0bdaf157d6fbf732eeb9d8b5d
Utkarsh singh <128299238+utkarshsingh-2004@users.noreply.github.com>
2024-03-11 19:30:21 +0530

    Add files via upload

---
+++ AdapterPatternDemo.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+ interface CreditCard{
+    public void giveBankDetails();
+    public String getCreditCard();
+}
+
+
+ class BankDetails{
+    private String bankName;
+    private String accHolderName;
+    private long accNumber;
+
+    public String getBankName(){
+        return bankName;
+    }
+    public void setBankName(String bankName){
+        this.bankName=bankName;
+    }
+    public String getAccHolderName(){
+        return accHolderName;
+    }
+    public void setAccHolderName(String accHolderName){
+        this.accHolderName=accHolderName;
+    }
+    public long getAccNumber(){
+        return accNumber;
+    }
+    public void setAccNumber(long accNumber){
+        this.accNumber=accNumber;
+    }
+}
+
+
+
+ class BankCustomer extends BankDetails implements CreditCard {
+    public void giveBankDetails(){
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Enter the account holder name: ");
+            String customername = br.readLine();
+            System.out.println("\n");
+
+            System.out.println("Enter the account number: ");
+            long accno = Long.parseLong(br.readLine());
+            System.out.println("\n");
+            
+            System.out.println("Enter the bank name: ");
+            String bankname = br.readLine();
+            setAccHolderName(customername);
+            setAccNumber(accno);
+            setBankName(bankname);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public String getCreditCard(){
+        long accno = getAccNumber();
+        String accholdername = getAccHolderName();
+        String bname = getBankName();
+
+        return("The Account number " + accno + " of " + accholdername + " in " + bname + " bank is valid and authenticated for issung the credit card.");
+    }
+}
+
+
+public class AdapterPatternDemo {
+    public static void main(String[] args) {
+        CreditCard tI = new BankCustomer();
+        tI.giveBankDetails();
+        System.out.println(tI.getCreditCard());
+    }
+}
