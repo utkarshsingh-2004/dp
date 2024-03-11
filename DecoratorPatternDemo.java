82a100e53722a8b76b3c756cac09f6c9c9efe5a5
Utkarsh singh <128299238+utkarshsingh-2004@users.noreply.github.com>
2024-03-11 19:41:44 +0530

    Add files via upload

---
+++ DecoratorPatternDemo.java
@@ -0,0 +1,62 @@
+interface Shape {
+    void draw();
+}
+
+class Rectangle implements Shape{
+    @Override
+    public void draw(){
+        System.out.println("Shape: Rectangle");
+    }
+}
+class Circle implements Shape{
+    @Override
+    public void draw(){
+        System.out.println("Shape: Circle");
+    }
+}
+
+abstract class ShapeDecorator implements Shape{
+    protected Shape decoratedShape;
+    public ShapeDecorator(Shape decoratedShape){
+        this.decoratedShape = decoratedShape;
+    }
+    public void draw(){
+        decoratedShape.draw();
+    }
+}
+
+class RedShapeDecorator extends ShapeDecorator{
+    public RedShapeDecorator(Shape decoratedShape) {
+        super(decoratedShape);
+    }
+    @Override
+    public void draw() {
+        decoratedShape.draw();
+        setRedBorder(decoratedShape);
+    }
+    private void setRedBorder(Shape decoratedShape){
+        System.out.println("Border Color: Red");
+    }
+}
+
+
+
+
+public class DecoratorPatternDemo {
+    public static void main(String[] args) {
+        Shape circle = new Circle();
+        Shape rectangle = new Rectangle();
+        Shape redCircle = new RedShapeDecorator(new Circle());
+        Shape redRecatngle = new RedShapeDecorator(new Rectangle());
+
+        System.out.println("Circle with Normal Border: ");
+        circle.draw();
+        System.out.println("\nRectangle with Normal Border: ");
+        rectangle.draw();
+
+        System.out.println("\nCircle with Red Border: ");
+        redCircle.draw();
+        System.out.println("\nRectangle with Red Border: ");
+        redRecatngle.draw();
+    }
+}
